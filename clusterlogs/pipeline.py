27d72425d03c646596d4f515b98deef4fd55bae6
maria <magsend@gmail.com>
2019-11-30 13:00:50 +0300

    small fixes

--- clusterlogs/pipeline.py
+++ clusterlogs/pipeline.py
@@ -114,13 +114,16 @@ class ml_clustering:
         tokens = Tokens(self.messages, type=self.tokenizer)
         self.tokenized = tokens.process()
         if self.w2v_size == 'auto':
-            # Based on https://developers.googleblog.com/2017/11/introducing-tensorflow-feature-columns.html
-            vocab = tokens.get_vocabulary()
-            embedding_size = round(len(vocab) ** 0.6)
-            if embedding_size >= 400:
-                embedding_size = 400
-            self.w2v_size = embedding_size
+            self.w2v_size = self.detect_embedding_size(tokens)
         return self
+
+
+    def detect_embedding_size(self, tokens):
+        vocab = tokens.get_vocabulary()
+        embedding_size = round(len(vocab) ** 0.6)
+        if embedding_size >= 400:
+            embedding_size = 400
+        return embedding_size
 
 
     @safe_run
@@ -279,32 +282,32 @@ class ml_clustering:
         else:
             return df.to_dict(orient='records')
 
+    @staticmethod
+    def distance_curve(distances, mode='show'):
+        """
+        Save distance curve with knee candidates in file.
+        :param distances:
+        :param mode: show | save
+        :return:
+        """
+        sensitivity = [1, 3, 5, 10, 100, 150]
+        knees = []
+        y = list(range(len(distances)))
+        for s in sensitivity:
+            kl = KneeLocator(distances, y, S=s)
+            knees.append(kl.knee)
 
-def distance_curve(distances, mode='show'):
-    """
-    Save distance curve with knee candidates in file.
-    :param distances:
-    :param mode: show | save
-    :return:
-    """
-    sensitivity = [1, 3, 5, 10, 100, 150]
-    knees = []
-    y = list(range(len(distances)))
-    for s in sensitivity:
-        kl = KneeLocator(distances, y, S=s)
-        knees.append(kl.knee)
-
-    plt.style.use('ggplot');
-    plt.figure(figsize=(10, 10))
-    plt.plot(distances, y)
-    colors = ['r', 'g', 'k', 'm', 'c', 'b', 'y']
-    for k, c, s in zip(knees, colors, sensitivity):
-        plt.vlines(k, 0, len(distances), linestyles='--', colors=c, label=f'S = {s}')
-    plt.legend()
-    if mode == 'show':
-        plt.show()
-    else:
-        plt.savefig("distance_curve.png")
+        plt.style.use('ggplot');
+        plt.figure(figsize=(10, 10))
+        plt.plot(distances, y)
+        colors = ['r', 'g', 'k', 'm', 'c', 'b', 'y']
+        for k, c, s in zip(knees, colors, sensitivity):
+            plt.vlines(k, 0, len(distances), linestyles='--', colors=c, label=f'S = {s}')
+        plt.legend()
+        if mode == 'show':
+            plt.show()
+        else:
+            plt.savefig("distance_curve.png")
 
 
 def remove_whitespaces(sentence):
--- test/test.py
+++ test/test.py
@@ -4,35 +4,33 @@ import pprint
 from clusterlogs import pipeline
 
 def main():
-    # df = pd.read_csv('error_logs_test.csv', index_col=0)
     df = pd.read_csv('test_data.csv', index_col=0)
     df.set_index('pandaid', inplace=True)
+    # To specify clustering parameters, please use dictionary:
     # clustering_parameters = {'tokenizer':'nltk',
-    #                          'w2v_size': 30,
+    #                          'w2v_size': 300,
     #                          'w2v_window': 10,
     #                          'min_samples': 1}
     target = 'exeerrordiag'
     mode = 'INDEX'
-    cluster = pipeline.ml_clustering(df, target, mode='process')
+    cluster = pipeline.ml_clustering(df, target, mode='create')
 
     cluster.process()
     output = cluster.clustered_output(mode)
     stats = cluster.statistics(output_mode='dict')
 
-    print(cluster.w2v_size)
-
     pprint.pprint(cluster.cluster_labels)
-    # pprint.pprint(output)
+    pprint.pprint(output)
     pprint.pprint(stats)
-    #
-    # pprint.pprint(cluster.in_cluster(1))
-    #
-    # pprint.pprint(cluster.messages_cleaned)
-    # pprint.pprint(cluster.tokenized)
-    # pprint.pprint(cluster.epsilon)
-    # pprint.pprint(cluster.get_w2v_vocabulary())
+
+    pprint.pprint(cluster.in_cluster(1))
+
+    pprint.pprint(cluster.messages_cleaned)
+    pprint.pprint(cluster.tokenized)
+    pprint.pprint(cluster.epsilon)
+    pprint.pprint(cluster.get_w2v_vocabulary())
     pprint.pprint(cluster.timings)
-    # cluster.distance_curve(cluster.distances, 'save')
+    cluster.distance_curve(cluster.distances, 'save')
 
 if __name__ == "__main__":
     sys.exit(main())
