0e40ce19fbf5eaeb010bd5a2e4b2093aa85144bd
maria <magsend@gmail.com>
2019-12-04 16:55:02 +0300

    updated version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="clusterlogs",
-    version="1.0.8",
+    version="1.0.9",
     author="Maria Grigorieva",
     author_email="magsend@gmail.com",
     description="Unsupervized Error Logs Clusterization",
