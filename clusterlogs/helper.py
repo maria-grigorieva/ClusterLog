e2da957f6577f5fa4152622fc579839b54eb05e3
maria <magsend@gmail.com>
2019-12-23 16:48:45 +0300

    updated regexp

--- clusterlogs/helper.py
+++ clusterlogs/helper.py
@@ -1,6 +1,16 @@
-from re import sub
+import re
 from kneed import KneeLocator
 import matplotlib.pyplot as plt
+
+_uid = r'[0-9a-zA-Z]{12,128}'
+_line_number = r'(at line[:]*\s*\d+)'
+_uuid = r'[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}'
+_url = r'(http[s]|root|srm|file)*:(//|/)(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+_filepath = "(/[a-zA-Z\./]*[\s]?)"
+path_regex = re.compile(r'(\b\w+://)\S+(?=\s)')
+file_regex = re.compile(r'(\b[f|F]ile( exists)?:?\s?)/\S+(?=\s)')
+py_regex = re.compile(r'/?\b[-./_a-zA-Z0-9]+\.py\b')
+long_regex = re.compile(r'[-./_a-zA-Z0-9]{25,}')
 
 def remove_whitespaces(sentence):
     """
@@ -19,17 +29,24 @@ def cleaner(messages):
     Removed parts of text are substituted with titles
     :return:
     """
-    _uid = r'[0-9a-zA-Z]{12,128}'
-    _line_number = r'(at line[:]*\s*\d+)'
-    _uuid = r'[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}'
 
     for idx, item in enumerate(messages):
-        _cleaned = sub(_line_number, "at line LINE_NUMBER", item)
-        _cleaned = sub(_uid, "UID", _cleaned)
-        _cleaned = sub(_uuid, "UUID", _cleaned)
-        messages[idx] = remove_whitespaces(_cleaned)
+        item = re.sub(_line_number, "at line LINE_NUMBER", item)
+        item = re.sub(_uid, "UID", item)
+        item = re.sub(_uuid, "UUID", item)
+        item = re.sub(_url, "URL", item)
+        item = re.sub("\d+", "NUM", item)
+        item = substitute_path(item)
+        messages[idx] = remove_whitespaces(item)
     return messages
 
+
+def substitute_path(string):
+    string = path_regex.sub(r'\1<PATH>', string)
+    string = py_regex.sub(r'<FILE.py>', string)
+    string = file_regex.sub(r'\1<FILE>', string)
+    string = long_regex.sub(r'LONG', string)
+    return string
 
 def distance_curve(distances, mode='show'):
     """
