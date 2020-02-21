e958e79be578144ce3df3d730f918ddcdd061cc3
maria <magsend@gmail.com>
2020-02-21 16:33:14 +0300

    added tfidf to train model

--- clusterlogs/training_model.py
+++ clusterlogs/training_model.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python
 import sys, getopt
-import re
+import numpy as np
 from gensim.models import Word2Vec
-from pyonmttok import Tokenizer
-from nltk.corpus import stopwords
-from string import punctuation
-from itertools import groupby
-
+from clusterlogs.tfidf import TermsAnalysis
+from clusterlogs.tokenization import Tokens
 
 def main(argv):
 
@@ -33,17 +30,18 @@ def main(argv):
 
     print('Log file contains {} lines'.format(len(messages)))
 
-    # Clean messages
-    messages_cleaned = [re.sub(r'([a-zA-Z_.|:;-]*\d+[a-zA-Z_.|:;-]*)+', '*', item) for item in messages]
-    #messages_cleaned = cleaning(messages)
-
-    # Tokenized cleaned messages
-    tokenized = tokenization(messages_cleaned)
-
-    cleaned = remove_neighboring_duplicates(tokenized)
+    tokens = Tokens(messages)
+    tokenized = tokens.remove_neighboring_duplicates(tokens.pyonmttok(Tokens.TOKENIZER_CLUSTER, messages))
+    vocab = tokens.get_vocabulary(tokenized)
+    print('Initial vocabulary size is {}'.format(len(vocab)))
+    tfidf = TermsAnalysis(tokenized)
+    cleaned = tfidf.process()
+    result = np.unique(cleaned)
+    vocab = tokens.get_vocabulary(result)
+    print('Cleaned vocabulary size is {}'.format(len(vocab)))
 
     try:
-        word2vec = Word2Vec(cleaned,
+        word2vec = Word2Vec(np.unique(cleaned),
                              size=300,
                              window=7,
                              min_count=1,
@@ -54,51 +52,6 @@ def main(argv):
     except Exception as e:
         print('Training model error')
 
-
-def tokenization(messages):
-    tokenized = []
-    for line in messages:
-        tokens, features = Tokenizer("conservative",
-                                     spacer_annotate=False,
-                                     preserve_placeholders=True)\
-            .tokenize(line)
-        tokenized.append(tokens)
-    return tokenized
-
-
-# def cleaning(messages):
-#     messages_cleaned = [0] * len(messages)
-#     for idx, item in enumerate(messages):
-#         item = re.sub(r'([ ])\1+', r'\1', item)
-#         item = re.sub(r'([* ])\1+', r'\1', item)
-#         item = re.sub(r'([a-zA-Z_.|:;-]*\d+[a-zA-Z_.|:;-]*)+', '｟*｠', item)
-#         messages_cleaned[idx] = item
-#     return messages_cleaned
-
-
-def remove_neighboring_duplicates(tokenized):
-    n = []
-    for row in tokenized:
-        remove_indices = [i - 1 for i, j in enumerate(row) if j == '｟*｠' and row[i - 1] == '▁']
-        row = [i for j, i in enumerate(row) if j not in remove_indices]
-        n.append([x for x, _ in groupby(row)])
-    return n
-
-
-def clean_tokens(tokenized):
-    """
-    Clean tokens from english stop words, numbers and punctuation
-    :return:
-    """
-    stop = stopwords.words('english') + list(punctuation) + ["``", "''"]
-    result = []
-    for row in tokenized:
-        tokenized = []
-        for i in row:
-            if i.lower() not in stop:
-                tokenized.append(i)
-        result.append(tokenized)
-    return result
 
 if __name__ == "__main__":
     main(sys.argv[1:])
