89f9706fefbbe82e0d75af924b80f4ba56465e67
DVGrin <dymong@yandex.ru>
2020-04-06 17:48:08 +0300

    Rewrote a couple of classes for clarity. Functionality was not changed. Also commented out unused methods in sequence_matching.py

--- clusterlogs/data_preparation.py
+++ clusterlogs/data_preparation.py
@@ -2,13 +2,13 @@ import re
 
 
 def clean_messages(messages):
-    messages_cleaned = [0] * len(messages)
-    for idx, item in enumerate(messages):
+    messages_cleaned = []
+    for item in messages:
         item = re.sub(r'\S+\.\S+', ' ', item)  # any URL
         item = re.sub(r'(/[\w\./]*\s?)', ' ', item)
         item = re.sub(r'([a-zA-Z0-9]+[_]+\S+)', ' ', item)
         item = re.sub(r'([a-zA-Z_.|:;-]*\d+[a-zA-Z_.|:;-]*)+', ' ', item)
         item = re.sub(r'[^\w\s]', ' ', item)
         item = re.sub(r' +', r' ', item)
-        messages_cleaned[idx] = item
+        messages_cleaned.append(item)
     return messages_cleaned
--- clusterlogs/sequence_matching.py
+++ clusterlogs/sequence_matching.py
@@ -1,77 +1,82 @@
-import nltk
-import numpy as np
-import difflib
-from itertools import groupby
 import random
+import difflib
+import numpy as np
+
+from itertools import chain
 from string import punctuation
+
+# import nltk
+# from itertools import groupby
 
 
 class Match:
-
-    def __init__(self, sequences):
+    '''
+    This class allows to extract the common pattern from a list of sequences.
+    Create a new Match object for every pattern extraction task.
+    '''
+    def __init__(self, sequences, match_threshhold=0.5, max_attempts=3):
         self.sequences = sequences
-
-    def matcher(self):
-        if len(self.sequences) > 1:
-            fdist = nltk.FreqDist([i for l in self.sequences for i in l])
-            # x = [token for token in lines[0] if (fdist[token] / len(lines) >= 1)]
-            x = [token if (fdist[token] / len(self.sequences) >= 1) else '(.*?)' for token in self.sequences[0]]
-            return [i[0] for i in groupby(x)]
-        else:
-            return self.sequences[0]
-
-    def matrix_matching(self):
-        x = list(map(list, zip(*self.sequences)))
-        return [tokens[0] if len(tokens) == 1 else '(.*?)' for tokens in
-                [np.unique(line) for line in x]]
+        self.match_threshhold = match_threshhold
+        self.max_attempts = max_attempts
+        self.attempt_number = 1
 
     def sequence_matcher(self, add_placeholder=False):
-        # number of attempts
-        max_attempts = 3
-        attempt = 1
-        # detect unique sentences
         unique = np.unique(self.sequences)
-        if len(unique) > 1:
-            pattern = random.choice(unique)
-            for i in range(1, len(unique)):
-                matches = difflib.SequenceMatcher(None, pattern, unique[i])
-                if matches.ratio() > 0.5:
-                    m = [pattern[m.a:m.a + m.size] for m
-                         in matches.get_matching_blocks() if m.size > 0]
-                    pattern = [val for sublist in m for val in sublist]
-                    if add_placeholder:
-                        x = [item + ['(.*?)'] if i < len(m) - 1 else item for i, item in enumerate(m)]
-                        pattern = [val for sublist in x for val in sublist]
-            # TODO:
-            # if pattern is empty - try to make it based on another sample message
-            is_empty = 0
-            for token in pattern:
-                if token in list(punctuation) or token == '(.*?)' or token == '▁':
-                    is_empty += 1
-            # is_empty = sum([True if token in list(punctuation) or token == '(.*?)' or
-            #                         token == '▁' else False for token in pattern])
-            if is_empty == len(pattern) and attempt <= max_attempts:
-                attempt += 1
-                print('Search for common pattern for {}. Next attempt...'.format(pattern))
-                self.sequence_matcher(add_placeholder)
-            else:
-                return pattern
-        else:
+        if len(unique) <= 1:
             return unique[0]
 
-    # def matching_clusters(self, sequences, patterns):
-    #     if len(sequences) > 0:
-    #         start = sequences[0]
-    #         similarities = self.levenshtein_similarity(start, sequences)
-    #         filtered = []
-    #         to_remove = []
-    #         for i, value in enumerate(similarities):
-    #             if value >= 0.6:
-    #                 filtered.append(sequences[i])
-    #                 to_remove.append(i)
-    #         sequences = np.delete(sequences, to_remove)
-    #         patterns.append(self.sequence_matcher(filtered))
-    #         while len(sequences) > 0:
-    #             self.matching_clusters(sequences, patterns)
-    #     else:
-    #         patterns = sequences[0]
+        pattern = random.choice(unique)
+        for sequence in unique:
+            matches = difflib.SequenceMatcher(None, pattern, sequence)
+            if matches.ratio() < self.match_threshhold:
+                continue
+
+            # We extract matching portions of sequences
+            # and change pattern to only contain those subsequences.
+            # In the end this gives us a common part of all the sequences.
+            match_ranges = matches.get_matching_blocks()[:-1]
+            matches = [pattern[m.a:m.a + m.size] for m in match_ranges]
+            if add_placeholder:  # Add a placeholder between matching subsequences
+                matches = [match + ['(.*?)'] for match in matches]
+                matches[-1].pop()
+            pattern = list(chain(*matches))  # concatenate inner lists
+
+        # TODO: if pattern is empty - try to make it based on another sample message
+        junk = list(punctuation) + ['_', '(.*?)', '']
+        is_empty = any(token for token in pattern if token not in junk)
+        if not is_empty or self.attempt_number > self.max_attempts:
+            return pattern
+        self.attempt_number += 1
+        print('Search for common pattern for {}. Next attempt...'.format(pattern))
+        self.sequence_matcher(add_placeholder)
+
+#    def matcher(self):
+#        if len(self.sequences) > 1:
+#            fdist = nltk.FreqDist([i for l in self.sequences for i in l])
+#            # x = [token for token in lines[0] if (fdist[token] / len(lines) >= 1)]
+#            x = [token if (fdist[token] / len(self.sequences) >= 1) else '(.*?)' for token in self.sequences[0]]
+#            return [i[0] for i in groupby(x)]
+#        else:
+#            return self.sequences[0]
+
+#    def matrix_matching(self):
+#        x = list(map(list, zip(*self.sequences)))
+#        return [tokens[0] if len(tokens) == 1 else '(.*?)' for tokens in
+#                [np.unique(line) for line in x]]
+
+#    def matching_clusters(self, sequences, patterns):
+#        if len(sequences) > 0:
+#            start = sequences[0]
+#            similarities = self.levenshtein_similarity(start, sequences)
+#            filtered = []
+#            to_remove = []
+#            for i, value in enumerate(similarities):
+#                if value >= 0.6:
+#                    filtered.append(sequences[i])
+#                    to_remove.append(i)
+#            sequences = np.delete(sequences, to_remove)
+#            patterns.append(self.sequence_matcher(filtered))
+#            while len(sequences) > 0:
+#                self.matching_clusters(sequences, patterns)
+#        else:
+#            patterns = sequences[0]
