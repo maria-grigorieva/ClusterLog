a0f43ec781287e26db34ca2b9d345ae8ec63b928
Dgrin <dymong@yandex.ru>
2020-11-15 17:14:10 +0300

    Fixed sequence matching, this time it works

--- clusterlogs/sequence_matching.py
+++ clusterlogs/sequence_matching.py
@@ -14,20 +14,26 @@ class Match:
     Create a new Match object for every pattern extraction task.
     '''
     def __init__(self, match_threshhold=0.8, add_placeholder=False):
-        #self.sequences = sequences
+        # self.sequences = sequences
         self.match_threshhold = match_threshhold
         self.add_placeholder = add_placeholder
 
     def sequence_matcher(self, sequences):
-        unique = np.unique(sequences).tolist()
-        if len(unique) <= 1:
-            return unique[0]
+        if len(sequences) <= 1:
+            return sequences[0]
 
+        # We have to do in this way because numpy.unique
+        # does not preserve types of sequence members and can
+        # transform an inner list into a string for some reason
+        unique = list(set([tuple(x) for x in sequences]))
+        unique = [list(x) for x in unique]
         random.shuffle(unique)
+
         for x in unique:
-            others = unique[:]
-            others.remove(x)
-            for sequence in others:
+            pattern = []
+            for sequence in unique:
+                if x == sequence:
+                    continue
                 matches = difflib.SequenceMatcher(None, x, sequence)
                 if matches.ratio() < self.match_threshhold:
                     continue
@@ -38,7 +44,7 @@ class Match:
                 match_ranges = matches.get_matching_blocks()[:-1]
                 matches = [x[m.a:m.a + m.size] for m in match_ranges]
                 if self.add_placeholder:  # Add a placeholder between matching subsequences
-                    [match + ['(.*?)'] for match in matches]
+                    matches = [match + ['(.*?)'] for match in matches]
                     matches[-1].pop()
                 pattern = list(chain(*matches))  # concatenate inner lists
 
@@ -55,20 +61,20 @@ class Match:
     # * SM picks random to compare to, this picks first one
     # * SM only compares sequences that have more similarity than self.match_threshhold
 
-    def matcher(self, sequences):
-        x = sequences[0]
-        for s in sequences:
-            matches = difflib.SequenceMatcher(None, x, s)
-            match_ranges = matches.get_matching_blocks()[:-1]
-            matches = [x[m.a:m.a + m.size] for m in match_ranges]
-            if self.add_placeholder:
-                matches = [match + ['(.*?)'] for match in matches]
-                matches[-1].pop()
-            pattern = list(chain(*matches))  # concatenate inner lists
-            junk = list(punctuation) + ['_', '(.*?)', '']
-            # if at least one of the items in sequence is not junk - return True
-            correct = any([token not in junk for token in pattern])
-        return pattern if correct else x
+    # def matcher(self, sequences):
+    #     x = sequences[0]
+    #     for s in sequences:
+    #         matches = difflib.SequenceMatcher(None, x, s)
+    #         match_ranges = matches.get_matching_blocks()[:-1]
+    #         matches = [x[m.a:m.a + m.size] for m in match_ranges]
+    #         if self.add_placeholder:
+    #             matches = [match + ['(.*?)'] for match in matches]
+    #             matches[-1].pop()
+    #         pattern = list(chain(*matches))  # concatenate inner lists
+    #         junk = list(punctuation) + ['_', '(.*?)', '']
+    #         # if at least one of the items in sequence is not junk - return True
+    #         correct = any([token not in junk for token in pattern])
+    #     return pattern if correct else x
 
     def matching_clusters(self, sequences, patterns):
         similarities = levenshtein_similarity_1_to_n(sequences)
@@ -80,7 +86,7 @@ class Match:
         if not filtered:
             filtered.append(sequences[0])
             to_remove.append(0)
-        patterns.append(self.matcher(filtered))
+        patterns.append(self.sequence_matcher(filtered))
         sequences = np.delete(sequences, to_remove)
         if len(sequences) > 1:
             self.matching_clusters(sequences, patterns)
