ac097dbca90bfdcc901e4a95cd9479d441a7fe65
Dgrin <dymong@yandex.ru>
2021-03-09 18:22:57 +0300

    Changed stylesheet to bootstrap 4 to work offline

--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,5 @@
 import dash
 import dash_bootstrap_components as dbc
 
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, title="ClusterLogs", external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, title="ClusterLogs")
