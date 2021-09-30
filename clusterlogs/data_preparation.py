e61bde617701aca4681131425f50e39d724b458f
Micol Olocco <64839981+micolocco@users.noreply.github.com>
2021-09-30 09:55:31 +0200

    massive regex cleaning

--- clusterlogs/data_preparation.py
+++ clusterlogs/data_preparation.py
@@ -1,21 +1,7 @@
 import re
 import pprint
 
-# def clean_messages(messages):
-#     messages_cleaned = []
-#     for item in messages:
-#         # print(item)
-#         item = re.sub(r'\S+\.\S+', ' ', item)  # any URL
-#         # item = re.sub(r'(/[\w\./]*\s?)', ' ', item)
-#         # item = re.sub(r'([a-zA-Z0-9]+[_]+\S+)', ' ', item)
-#         item = re.sub(r'([a-zA-Z_.|:;-]*\d+[a-zA-Z_.|:;-]*)+', ' ', item) # remove all substrings with digits
-#         item = re.sub(r'(\d+)', ' ', item) # remove all other digits
-#         item = re.sub(r'[^\w\s]', ' ', item) # removes all punctuation
-#         item = re.sub(r' +', r' ', item)
-#         messages_cleaned.append(item.lower())
-#         # print(item)[A-Z_]{2,}
-#     # pprint.pprint(messages_cleaned)
-#     return messages_cleaned
+
 def pre_cleaning(messages):
     messages_cleaned=[]
     for message in messages:
